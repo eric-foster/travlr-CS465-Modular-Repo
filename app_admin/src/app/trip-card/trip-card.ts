0c506d4dcba105d5268ea72acd1ab3f22857a050
Eric Foster <eric.foster3@snhu.edu>
2025-12-13 13:41:59 -0500

    Added an Admin page using the SPA Architecture and added CRUD Functionality - Module 6 Completed

---
+++ app_admin/src/app/trip-card/trip-card.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Trip } from '../models/trip';
+
+@Component({
+  selector: 'app-trip-card',
+  imports: [CommonModule],
+  templateUrl: './trip-card.html',
+  styleUrl: './trip-card.css',
+})
+
+export class TripCard implements OnInit {
+  @Input('trip') trip: any;
+  @Output() delete = new EventEmitter<Trip>();
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+
+  }
+
+  public editTrip(trip: Trip) {
+    localStorage.removeItem('tripCode');
+    localStorage.setItem('tripCode', trip.code);
+    this.router.navigate(['edit-trip']);
+  }
+
+  public deleteTrip(trip: Trip): void {
+    if (!confirm(`Are you sure you want to delete trip ${trip.code}?`)) {
+      return;
+    }
+
+    this.delete.emit(this.trip);
+  }
+}
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,9 +4,19 @@ const router = express.Router();
 const tripsController = require('../controllers/trips');
 
 // GET: All trips in the list
-router.route("/trips").get(tripsController.tripsList);
+// POST: Add a trip to the list
+router
+    .route("/trips")
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
 // GET: A single Trip by a Code - Requires Parameter
-router.route("/trips/:tripCode").get(tripsController.tripsFindByCode);
+// PUT: Update a specific trip - Requires Parameters
+// DELETE: Remove a specific trip - Requires Parameter
+router
+    .route("/trips/:tripCode")
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip)
+    .delete(tripsController.tripsDeleteTrip);
 
 module.exports = router;
